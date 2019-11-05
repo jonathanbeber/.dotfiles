872c4165eff9068f5dd9419c206e6197b23ff044
Jonathan Juares Beber <jonathanbeber@gmail.com>
2019-11-05 10:06:58 +0100

    Add clippy and cargo edit tooling
    Cargo edit and clippy are two tools indicated by the pingcap talent plan
    course on pratical network applicatoins.
    
    This commit installs both tools.

--- tools/rust/install.sh
+++ tools/rust/install.sh
@@ -5,3 +5,12 @@ bash <(curl https://sh.rustup.rs -sSf) -y
 
 # rustfmt
 rustup component add rustfmt
+
+# clippy
+rustup component add clippy
+
+# cargo edit
+sudo apt-get -y install \
+    pkg-config \
+    libssl-dev
+cargo install cargo-edit
