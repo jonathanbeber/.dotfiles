edc47fcb5e2be58f253f6411dabd77fd5e8fc87c
Jonathan Juares Beber <jonathanbeber@gmail.com>
2021-11-29 20:18:41 +0100

    upgrade codium installation

--- tools/code/install.sh
+++ tools/code/install.sh
@@ -3,19 +3,22 @@
 source utils.sh
 
 # https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo
-wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | sudo apt-key add -
-echo 'deb https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs/ vscodium main' | sudo tee /etc/apt/sources.list.d/vscodium.list
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+    | gpg --dearmor \
+    | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs vscodium main' \
+    | sudo tee /etc/apt/sources.list.d/vscodium.list
 
 sudo apt-get -y update
 sudo apt-get -y install codium
 
 # Extensions
-codium --install-extension rust-lang.rust
-codium --install-extension ms-python.python
-codium --install-extension ms-vscode.go
-codium --install-extension travisthetechie.write-good-linter
-codium --install-extension patrykpeszko.vscode-proselint
-codium --install-extension  valentjn.vscode-ltex
+codium --force --install-extension rust-lang.rust
+codium --force --install-extension ms-python.python
+codium --force --install-extension ms-vscode.go
+codium --force --install-extension travisthetechie.write-good-linter
+codium --force --install-extension patrykpeszko.vscode-proselint
+codium --force --install-extension valentjn.vscode-ltex
 
 # configs
 _ln "$(pwd)/tools/code/settings.json" "${HOME}/.config/VSCodium/User/settings.json"
