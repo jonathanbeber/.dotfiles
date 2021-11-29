864f8e2c78b81b7dac17d6cd27d54bcd07798f9a
Jonathan Juares Beber <jonathanbeber@gmail.com>
2021-11-29 20:18:07 +0100

    aws cli v2

--- tools/awscli/install.sh
+++ tools/awscli/install.sh
@@ -2,7 +2,15 @@
 
 source utils.sh
 
-pip3 install awscli --upgrade --user
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
+unzip  -o /tmp/awscliv2.zip -d /tmp/
+if which aws; then
+    sudo /tmp/aws/install --upgrade
+else
+    sudo /tmp/aws/install
+fi
+
 
 # configs
+test -d "${HOME}/.aws" || mkdir -p $_
 _ln "$(pwd)/tools/awscli/config" "${HOME}/.aws/config"
