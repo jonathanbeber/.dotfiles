58a6a98ee5194151b057b0ecced348e95a111c0b
Jonathan Juares Beber <jonathanbeber@gmail.com>
2021-12-01 08:50:33 +0100

    install [la]tex and texworks

--- install.sh
+++ install.sh
@@ -27,6 +27,7 @@ sudo apt-get -y install \
 # Install tools
 bash tools/git/install.sh
 bash tools/code/install.sh
+bash tools/tex/install.sh
 bash tools/i3/install.sh
 bash tools/rust/install.sh
 bash tools/python/install.sh
---
+++ tools/tex/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# tex and latex
+sudo apt-get -y install texlive texlive-latex-extra
+
+# editor
+sudo apt-get -y install texworks
