1d6b6bb13170c15bceaa30e730772a6e0d150ada
Jonathan Juares Beber <jonathanbeber@gmail.com>
2021-12-01 08:55:28 +0100

    better handling broken links and existing config files

--- utils.sh
+++ utils.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
 _ln() {
-    test -f $2 || ln -s $1 $2
+    if [[ -f $2 ]]; then
+        if [[ ! -L $2 ]]; then
+            mv $2 "${2}.bckp"
+            echo "warn: original file moved to ${2}.bckp" 1>&2
+        else
+            return
+        fi
+    else
+        test -L $2 && rm $2 && echo "warn: broken link found and removed ${2}" 1>&2
+    fi
+    ln -s $1 $2
 }
