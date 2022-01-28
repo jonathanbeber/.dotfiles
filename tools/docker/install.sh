18c5cf868621d09bc310e62e55412cfc0ff8cdd4
Jonathan Juares Beber <jonathanbeber@gmail.com>
2022-01-28 14:35:14 +0100

    install docker-compose

--- tools/docker/install.sh
+++ tools/docker/install.sh
@@ -5,3 +5,7 @@ sudo apt-get -y install docker.io
 sudo usermod -aG docker ${USER}
 
 sudo systemctl enable docker
+
+# Compose
+asdf plugin-add docker-compose https://github.com/jeebak/asdf-docker-compose.git
+asdf install docker-compose 1.29.2
